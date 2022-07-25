5000fbbfa12d40342800081bbcf58af712e5d0ca
LuccasLpn <Luccas.lpn@outlook.com>
2022-07-25 11:29:48 -0300

    start project

---
+++ JwtImpl/src/test/java/br/com/jwtimpl/JwtImplApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jwtimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
